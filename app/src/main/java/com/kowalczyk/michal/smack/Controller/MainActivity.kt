587b43ee3fcc111e8f3cc607eff2e971c1e56933
Michał Kowalczyk <lavos1996@o2.pl>
2019-01-02 16:17:31 +0100

    Lesson 9 Avatar i jego tlo przy zalogowaniu broadcast i szlify

--- app/src/main/java/com/kowalczyk/michal/smack/Controller/MainActivity.kt
+++ app/src/main/java/com/kowalczyk/michal/smack/Controller/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.kowalczyk.michal.smack.Controller
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.kowalczyk.michal.smack.R
+import com.kowalczyk.michal.smack.Services.AuthService
+import com.kowalczyk.michal.smack.Services.UserDataService
+import com.kowalczyk.michal.smack.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 
 //to jest potrzebne(w deklaracji klasy) do zakomentowanych czesci kodu NavigationView.OnNavigationItemSelectedListener
@@ -37,6 +46,29 @@ class MainActivity : AppCompatActivity(){
 
         //to jest odpowiedzialne za to co sie stanie jak klikniemy w jakas opcje z tego menu co sie wysuwa od lewej
         /*nav_view.setNavigationItemSelectedListener(this)*/
+
+        //Odbiornik broadcasta
+        //IntentFIlter bedzie mowil jakiego typu Intenta ma szukac/oczekiwac
+        LocalBroadcastManager.getInstance(this).registerReceiver(userDataCahngeReceiver, IntentFilter(
+            BROADCAST_USER_DATA_CHANGE))
+
+    }
+
+    //BROADCAST RECEIVER
+    private val userDataCahngeReceiver=object:BroadcastReceiver(){
+        override fun onReceive(context: Context?, intent: Intent?) {
+            //co sie ma zadziac jak sygnal zostanie odebrany
+            //AuthService to singleton ktorego stworzylismy a isLoggedIn jest jego czescia
+            if(AuthService.isLoggedIn){
+                //jesli jest zalogowany uzytkownik to podmieniamy jego logo nazwe itp
+                userNameNavHeader.text=UserDataService.name
+                userEmailNavHeader.text=UserDataService.email
+                val resourceId=resources.getIdentifier(UserDataService.avatarName,"drawable",packageName)
+                userImageNavHeader.setImageResource(resourceId)
+                userImageNavHeader.setBackgroundColor(UserDataService.returnAvatarColor(UserDataService.avatarColor))
+                loginBtnNavHeader.text="Logout"
+            }
+        }
     }
 
     override fun onBackPressed() {
@@ -93,8 +125,19 @@ class MainActivity : AppCompatActivity(){
     fun loginBtnNavClicked(view: View){
         //Intent() 1 parametr context drugi parametr gdzie to wysyłamy nazwa::class.java
         //explicit intent
-        val loginIntent=Intent(this, LoginActivity::class.java)
-        startActivity(loginIntent)
+
+        if(AuthService.isLoggedIn){
+            //logout
+            UserDataService.logout()
+            userNameNavHeader.text="Login"
+            userEmailNavHeader.text=""
+            userImageNavHeader.setImageResource(R.drawable.profiledefault)
+            userImageNavHeader.setBackgroundColor(Color.TRANSPARENT)
+            loginBtnNavHeader.text="Login"
+        }else{
+            val loginIntent=Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
     }
 
     fun addChannelClicked(view:View){
--- app/src/main/java/com/kowalczyk/michal/smack/Services/UserDataService.kt
+++ app/src/main/java/com/kowalczyk/michal/smack/Services/UserDataService.kt
@@ -1,10 +1,57 @@
 package com.kowalczyk.michal.smack.Services
 
+import android.graphics.Color
+import java.util.*
+
 object UserDataService {
     var id=""
     var avatarColor=""
     var avatarName=""
     var email=""
     var name=""
+
+
+    fun logout(){
+        id=""
+        avatarColor=""
+        avatarName=""
+        email=""
+        name=""
+        AuthService.authToken=""
+        AuthService.userEmail=""
+        AuthService.isLoggedIn=false
+    }
+
+
+    fun returnAvatarColor(components:String):Int{
+
+        //[0.4980392156862745, 0.596078431372549, 0.8901960784313725, 1]
+
+        //musimy pozbyc sie nawiasow i przecinkow
+
+        // 0.4980392156862745 0.596078431372549 0.8901960784313725 1
+
+        val strippedColor=components
+            .replace("[","")
+            .replace("]","")
+            .replace(",","")
+
+        var r=0
+        var g=0
+        var b=0
+
+        //tworzymy skaner ktory posluzy do tego zeby w tym stringu znalazl i wycial liczby typu double
+        val scanner=Scanner(strippedColor)
+
+        if(scanner.hasNext()){
+            //pobiermay wartosc ze stringa a nastepniemy mnozymy przez 255 zeby otrzymac liczbe z zakresu od 0 do 255 i rzutujemy na int
+            r=(scanner.nextDouble()*255).toInt()
+            g=(scanner.nextDouble()*255).toInt()
+            b=(scanner.nextDouble()*255).toInt()
+        }
+
+        //Color jest w rzeczywistosci Intem
+        return Color.rgb(r,g,b)
+    }
 
 }
